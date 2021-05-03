9c6b49bf4e7c6bacda52eea309a48a7d5d6d906d
aiYangYang <752249120@qq.com>
2021-05-03 23:33:20 +0800

    fix: add graph search question

---
+++ sort/selectionSort.js
@@ -0,0 +1,17 @@
+Array.prototype.selectionSort = function() {
+    for(let i = 0; i< this.length-1;i++) {
+        let indexMin = i;
+        for(let j = i; j<this.length;j++) {
+            if(this[j] < this[indexMin]) {
+                indexMin = j;
+            }
+        }
+        if(indexMin !== i) {
+            const temp = this[i];
+            this[i] = this[indexMin];
+            this[indexMin] = temp;
+        }
+    }
+}
+const arr = [5,4,3,2,1];
+arr.selectionSort();

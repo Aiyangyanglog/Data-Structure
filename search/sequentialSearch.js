9fecba79954d8ee0f16712bda6c4a0233d2eec2a
ayy <752249120@qq.com>
2022-08-25 16:59:56 +0800

    fix: add search

--- search/sequentialSearch.js
+++ search/sequentialSearch.js
@@ -1,9 +1,10 @@
-Array.prototype.sequentialSearch = function(item) {
-    for(let i = 0;i<this.length;i++) {
-        if(this[i] == item) {
-            return i
-        }
+Array.prototype.sequentialSearch = function (item) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] == item) {
+      return i;
     }
-    return -1;
-}
-const res = [1,2,3,4,5].sequentialSearch(3);
+  }
+  return -1;
+};
+const res = [1, 2, 3, 4, 5].sequentialSearch(3);
+console.log(res);

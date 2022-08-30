6c6b040bd2c3c24e12cca1dfcb51033a920b9c16
ayy <752249120@qq.com>
2022-08-30 11:02:43 +0800

    fix: change set

--- set/349.两个数组的交集.js
+++ set/349.两个数组的交集.js
@@ -3,6 +3,7 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+// 时间复杂度O(m*n) 空间复杂度O(m)
 var intersection = function (nums1, nums2) {
   return [...new Set(nums1)].filter((n) => nums2.includes(n));
 };
